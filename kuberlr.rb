0ca7b3b424e46229acc056ecff7e2766336686ff
Micael Filipe <10292135+notmicaelfilipe@users.noreply.github.com>
2025-12-07 17:42:45 +0000

    update kuberlr

--- kuberlr.rb
+++ kuberlr.rb
@@ -2,7 +2,7 @@
 class Kuberlr < Formula
     desc "Kuberlr - A tool that simplifies the management of multiple versions of kubectl."
     baseurl = "https://github.com/flavio/kuberlr/releases/download"
-    version "0.6.0"
+    version "0.6.1"
 
     livecheck do
       url :stable
@@ -12,23 +12,23 @@ class Kuberlr < Formula
     on_macos do
       on_arm do
         kernel = "darwin"
-        sha256 "362a993f52048845c19385a2eeb9a8827fa9ae46daec2e8ebdfc962270db5bbd"
+        sha256 "a02f536019efd8096b2145d3d5a7762f2a8f14d7a6abfbac503e35e24293bac9"
         arch = "arm64"
-        url baseurl + "/v0.6.0/" + "kuberlr_0.6.0_" + kernel + "_" + arch + ".tar.gz"
+        url baseurl + "/v0.6.1/" + "kuberlr_0.6.1_" + kernel + "_" + arch + ".tar.gz"
       end
       on_intel do
         kernel = "darwin"
-        sha256 "67fdef2cef62e7e4a3c5145e4bfc736a988c0f4ffe0c1bba84c8b61510e92c5d"
+        sha256 "cf2ba65e2f0bfd13f96187654aab03f7e240d871a222f248078eba94b275bf5e"
         arch = "amd64"
-        url baseurl + "/v0.6.0/" + "kuberlr_0.6.0_" + kernel + "_" + arch + ".tar.gz"
+        url baseurl + "/v0.6.1/" + "kuberlr_0.6.1_" + kernel + "_" + arch + ".tar.gz"
       end
     end
 
     on_linux do
       kernel = "linux"
-      sha256 "8e3519b86a0e459b923e12da38c80ef9e73172cdd14b6086eb613c78058d2993"
+      sha256 "10874d9097c6c2657a0e06395fc321daa0060f18581a1de6ea6526625e2f124f"
       arch = "amd64"
-      url baseurl + "/v0.6.0/" + "kuberlr_0.6.0_" + kernel + "_" + arch + ".tar.gz"
+      url baseurl + "/v0.6.1/" + "kuberlr_0.6.1_" + kernel + "_" + arch + ".tar.gz"
     end
 
     @@bin = "kuberlr"
